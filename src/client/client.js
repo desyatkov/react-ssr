b490cbf6e74436636be88e11d3361a6f3a1f5d44
Sergey Desyatkov <Desyatkov@gmail.com>
2018-02-18 23:55:49 +0200

    init

---
+++ src/client/client.js
@@ -0,0 +1,3 @@
+// Startup point for client side app
+
+console.log('Hi there');
---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>I'm the home  Best component</div>
+      <button onClick={()=>console.log('Hy there')}>Press me</button>
+    </div>
+  );
+};
+
+export default Home;
